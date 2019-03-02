fec7614c18b6b81d1afba48aa1fb7de26e8da621
Fluffy Doggo <38565476+FluffyDoggoOfficial@users.noreply.github.com>
2019-03-02 17:01:01 -0600

    Create ItemBase.java

---
+++ src/main/java/com/fluffydoggomods/scplockdownextras/items/ItemBase.java
@@ -0,0 +1,29 @@
+package com.fluffydoggomods.scplockdownextras.items;
+
+import com.fluffydoggomods.scplockdownextras.Main;
+import com.fluffydoggomods.scplockdownextras.init.ModItems;
+import com.fluffydoggomods.scplockdownextras.tabs.ScpLeTab;
+import com.fluffydoggomods.scplockdownextras.util.IHasModel;
+
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class ItemBase extends Item implements IHasModel {
+	public ItemBase(String name, CreativeTabs tab) {
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		if (tab != null) {
+			setCreativeTab(Main.scpletab);
+		} else {
+			setCreativeTab(tab);
+		}
+		
+		ModItems.ITEMS.add(this);
+	}
+	@Override
+	public void registerModels() {
+		Main.proxy.registerItemRenderer(this, 0, "inventory");
+	}
+
+}
