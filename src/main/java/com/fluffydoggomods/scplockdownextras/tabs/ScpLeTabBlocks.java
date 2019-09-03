8840422b16f823452eda794ee8b0b9e9a66fe2fa
Fluffy Doggo <38565476+FluffyDoggoOfficial@users.noreply.github.com>
2019-09-03 07:30:08 -0500

    Tab Changes
    Renamed the Main tab to the Blocks Tab
    Added the SCPs Tab
    Added the Items Tab

--- src/main/java/com/fluffydoggomods/scplockdownextras/tabs/ScpLeTabBlocks.java
+++ src/main/java/com/fluffydoggomods/scplockdownextras/tabs/ScpLeTabBlocks.java
@@ -1,19 +1,20 @@
 package com.fluffydoggomods.scplockdownextras.tabs;
 
+import com.fluffydoggomods.scplockdownextras.init.ModBlocks;
 import com.fluffydoggomods.scplockdownextras.init.ModItems;
 
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
 
-public class ScpLeTabDocuments extends CreativeTabs
+public class ScpLeTabBlocks extends CreativeTabs
 {
-	public ScpLeTabDocuments()
+	public ScpLeTabBlocks()
 	{
-		super("scpletabdocs");
+		super("scpletabblocks");
 	}
 	
 	public ItemStack getTabIconItem()
 	{
-		return new ItemStack(ModItems.DOCUMENT);
+		return new ItemStack(ModBlocks.METAL_PANEL_2);
 	}
 }
